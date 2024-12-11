5f897ab1716fd26c0e5f296ed6b8407246c488f7
vova <vladyegorin@gmail.com>
2024-12-10 22:03:08 -0800

    first commit

---
+++ src/main/java/vladyegorin/Bot.java
@@ -0,0 +1,64 @@
+package vladyegorin;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Bot extends TelegramLongPollingBot {
+
+    private String botToken;
+    @Override
+    public String getBotUsername() {
+        return "chatgpttelegrammmm_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    public Bot() {
+        // Load properties from config file in resources folder
+        Properties properties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new FileNotFoundException("config.properties not found in resources folder.");
+            }
+            properties.load(input);
+            botToken = properties.getProperty("TELEGRAM_BOT_TOKEN");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1); // Exit if the configuration file can't be loaded
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            var msg = update.getMessage();
+            var user = msg.getFrom();
+            var id = user.getId();
+
+            System.out.println("\nNew message!");
+            System.out.println("User ID: " + id);
+            System.out.println("Username: " + user.getUserName());
+
+
+
+            if (msg.hasText()) {
+                System.out.println("Text message: " + msg.getText());
+
+            } else {
+                //IDontUnderstand(msg, id);
+            }
+        }
+    }
+
+
+
+
+}
